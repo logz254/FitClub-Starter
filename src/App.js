ef1a5401330c888356159e39f09fc96fd6fd0b00
Allan <Allanlogz@gmail.com>
2022-09-02 13:26:41 +0300

    Adding Hero,Plans,Programs,Reasons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import Hero from './components/Hero/Hero';
+import Programs from './components/Programs/Programs';
+import Reasons from './components/Reasons/Reasons';
+import Plans from './components/Plans/Plans';
 function App() {
   return (
     <div className="App">
-          <span style={{color: 'white'}}>Subscribe Zainkeepscode</span>
+         <Hero/>
+         <Programs/>
+         <Reasons/>
+         <Plans/>
     </div>
   );
 }
