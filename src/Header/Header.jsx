f751d38af225e4932bcf7a35f80557425a25803c
Allan <Allanlogz@gmail.com>
2022-09-05 13:53:25 +0300

    Adding AutoScroll

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import './Header.css'
-import Logo from '../assets/logo.png'
-import Bars from '../assets/bars.png'
+import React from 'react';
+import './Header.css';
+import Logo from '../assets/logo.png';
+import Bars from '../assets/bars.png';
+import { Link } from 'react-scroll';
 import { useState } from 'react'
 const Header = () => {
   const mobile = window.innerWidth<=768?true:false
@@ -17,11 +18,45 @@ const Header = () => {
             </div>
         ):(
         <ul className='header-menu'>
-        <li onClick={()=>setMenuOpened(false)}>Home</li>
-        <li onClick={()=>setMenuOpened(false)}>Programs</li>
-        <li onClick={()=>setMenuOpened(false)}>Why Us</li>
-        <li onClick={()=>setMenuOpened(false)}>Plans</li>
-        <li onClick={()=>setMenuOpened(false)}>Testimonials</li>
+        <li>
+          <Link
+          onClick={()=>setMenuOpened(false)}
+          activeClass="active"
+          to='header'
+          span={true}
+          smooth={true}
+          >Home</Link>
+          </li>
+        <li>
+          <Link
+          onClick={()=>setMenuOpened(false)}
+          to="programs"
+          spy={true}
+          smooth={true}
+          >
+          Programs</Link></li>
+        <li>
+          <Link
+        onClick={()=>setMenuOpened(false)}
+        to="reasons"
+        spy={true}
+        smooth={true}
+        >
+        Why us
+        </Link></li>
+        <li><Link
+        onClick={()=>setMenuOpened(false)}
+        to="plans"
+        spy={true}
+        smooth={true}
+        >
+          Plans</Link></li>
+        <li><Link
+        onClick={()=>setMenuOpened(false)}
+        to='testimonials'
+        span={true}
+        smooth={true}
+        >Testimonials</Link></li>
     </ul>
     )}
         
