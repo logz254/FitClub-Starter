da9da54445e76c709020eaeb718a2a9dbefbc03f
Allan <Allanlogz@gmail.com>
2022-09-05 14:00:21 +0300

    Adjusted counting numbers and fixed responsiveness

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,6 +5,7 @@ import hero_image from "../../assets/hero_image.png";
 import hero_image_back from "../../assets/hero_image_back.png";
 import Heart from "../../assets/heart.png";
 import Calories from "../../assets/calories.png";
+import NumberCounter from 'number-counter';
 import {motion} from 'framer-motion';
 
 const Hero = () => {
@@ -36,11 +37,29 @@ const Hero = () => {
           </div>
           {/*figures*/}
           <div className="figures">
-            <div><span>+140</span><span>expert coaches</span></div>
+            <div>
+              <span>
+                <NumberCounter end={140} start={100} delay='4' preFix="+"/>
+              </span>
+              <span>
+                expert coaches
+                </span></div>
 
-          <div><span>+978</span><span>members joined</span></div>
+          <div>
+            <span>
+            <NumberCounter end={978} start={800} delay='4' preFix="+"/>
+            </span>
+            <span>
+              members joined
+              </span></div>
 
-          <div><span>+50</span><span>fitness programs</span></div>
+          <div>
+            <span>
+            <NumberCounter end={50} start={0} delay='4' preFix="+"/>
+            </span>
+            <span>
+              fitness programs
+              </span></div>
 
           </div>
           {/*hero buttons*/}
